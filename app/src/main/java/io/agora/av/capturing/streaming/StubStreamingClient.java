0aba4d8de5ed0a2d91ab3e18b95e976178c07da8
sonuan <isonuan@gmail.com>
2017-10-16 14:24:20 +0800

    init qiniu rtc

--- app/src/main/java/io/agora/av/capturing/streaming/StubStreamingClient.java
+++ app/src/main/java/io/agora/av/capturing/streaming/StubStreamingClient.java
@@ -1,26 +1,47 @@
 package io.agora.av.capturing.streaming;
 
+import com.qiniu.pili.droid.rtcstreaming.RTCStreamingManager;
+import com.qiniu.pili.droid.streaming.av.common.PLFourCC;
+
+import io.agora.av.capturing.streaming.ui.LiveRoomActivity;
+
+
 /**
  * Implement your own streaming client
  */
 public class StubStreamingClient extends StreamingClient {
+    RTCStreamingManager mRTCStreamingManager;
+    private final LiveRoomActivity mLiveRoomActivity;
+
+    public StubStreamingClient(RTCStreamingManager rtcStreamingManager, LiveRoomActivity activity) {
+        mRTCStreamingManager = rtcStreamingManager;
+        mLiveRoomActivity = activity;
+    }
     @Override
     public void startStreaming() {
         // do nothing
+        mLiveRoomActivity.startPublishStreaming();
     }
 
     @Override
     public void stopStreaming() {
         // do nothing
+        mLiveRoomActivity.stopPublishStreaming();
     }
 
     @Override
     public void sendYUVData(final byte[] yuv, final int width, final int height) {
         // do nothing
+        if (mRTCStreamingManager.isStreamingStarted() && mLiveRoomActivity.isPublishStreamStarted()) {
+            mRTCStreamingManager.inputVideoFrame(yuv, width, height, 90, false, PLFourCC.FOURCC_I420, System.nanoTime());
+        }
     }
 
     @Override
     public void sendPCMData(final byte[] pcm) {
         // do nothing
+        if (mRTCStreamingManager.isStreamingStarted() && mLiveRoomActivity.isPublishStreamStarted()) {
+            mRTCStreamingManager.inputAudioFrame(pcm, System.nanoTime());
+        }
     }
 }
--- app/src/main/java/io/agora/av/capturing/streaming/ui/LiveRoomActivity.java
+++ app/src/main/java/io/agora/av/capturing/streaming/ui/LiveRoomActivity.java
@@ -1,12 +1,20 @@
 package io.agora.av.capturing.streaming.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
+import android.hardware.Camera;
 import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
@@ -16,11 +24,29 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.qiniu.pili.droid.rtcstreaming.RTCConferenceOptions;
+import com.qiniu.pili.droid.rtcstreaming.RTCConferenceState;
+import com.qiniu.pili.droid.rtcstreaming.RTCConferenceStateChangedListener;
+import com.qiniu.pili.droid.rtcstreaming.RTCRemoteWindowEventListener;
+import com.qiniu.pili.droid.rtcstreaming.RTCStreamingManager;
+import com.qiniu.pili.droid.rtcstreaming.RTCUserEventListener;
+import com.qiniu.pili.droid.rtcstreaming.RTCVideoWindow;
+import com.qiniu.pili.droid.streaming.AVCodecType;
+import com.qiniu.pili.droid.streaming.StreamStatusCallback;
+import com.qiniu.pili.droid.streaming.StreamingProfile;
+import com.qiniu.pili.droid.streaming.StreamingSessionListener;
+import com.qiniu.pili.droid.streaming.StreamingState;
+import com.qiniu.pili.droid.streaming.StreamingStateChangedListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.List;
 
 import io.agora.av.capturing.streaming.R;
 import io.agora.av.capturing.streaming.StubStreamingClient;
@@ -125,8 +151,8 @@ public class LiveRoomActivity extends BaseActivity implements AGEventHandler {
         LinearLayout bottomContainer = (LinearLayout) findViewById(R.id.bottom_container);
         FrameLayout.MarginLayoutParams fmp = (FrameLayout.MarginLayoutParams) bottomContainer.getLayoutParams();
         fmp.bottomMargin = virtualKeyHeight() + 16;
-
-        mPreProcessing.setStreamingClient(new StubStreamingClient()); // TODO Implement your own streaming client
+        initQiniuRtc();
+        mPreProcessing.setStreamingClient(new StubStreamingClient(mRTCStreamingManager, this)); // TODO Implement your own streaming client
         mPreProcessing.registerPreProcessing();
     }
 
@@ -521,5 +547,385 @@ public class LiveRoomActivity extends BaseActivity implements AGEventHandler {
         } else {
             iv.clearColorFilter();
         }
+    }
+
+    private static final String TAG = "QiniuRtc";
+    private RTCStreamingManager mRTCStreamingManager;
+    private StreamingProfile mStreamingProfile;
+    private boolean isSwCodec = false;
+    private boolean mRole = true;
+    private boolean mIsActivityPaused = false;
+    private boolean mIsPublishStreamStarted = false;
+    private boolean mIsInReadyState = false;
+
+    private void initQiniuRtc() {
+        RTCStreamingManager.init(getApplicationContext());
+
+        AVCodecType codecType = isSwCodec ? AVCodecType.SW_VIDEO_WITH_SW_AUDIO_CODEC : AVCodecType.HW_VIDEO_YUV_AS_INPUT_WITH_HW_AUDIO_CODEC;
+        mRTCStreamingManager = new RTCStreamingManager(getApplicationContext(), codecType);
+        mRTCStreamingManager.setConferenceStateListener(mRTCStreamingStateChangedListener);
+        mRTCStreamingManager.setRemoteWindowEventListener(mRTCRemoteWindowEventListener);
+        mRTCStreamingManager.setStreamingStateListener(mStreamingStateChangedListener);
+        mRTCStreamingManager.setUserEventListener(mRTCUserEventListener);
+        mRTCStreamingManager.setDebugLoggingEnabled(false);
+
+        RTCConferenceOptions options = new RTCConferenceOptions();
+        mRole = isBroadcaster(getIntent().getIntExtra(ConstantApp.ACTION_KEY_CROLE, Constants.CLIENT_ROLE_BROADCASTER));
+        if (mRole) {
+            // anchor should use a bigger size, must equals to `StreamProfile.setPreferredVideoEncodingSize` or `StreamProfile.setEncodingSizeLevel`
+            // RATIO_16_9 & VIDEO_ENCODING_SIZE_HEIGHT_480 means the output size is 848 x 480
+            options.setVideoEncodingSizeRatio(RTCConferenceOptions.VIDEO_ENCODING_SIZE_RATIO.RATIO_16_9);
+            options.setVideoEncodingSizeLevel(RTCConferenceOptions.VIDEO_ENCODING_SIZE_HEIGHT_480);
+            options.setVideoBitrateRange(300 * 1024, 800 * 1024);
+            // 15 fps is enough
+            options.setVideoEncodingFps(15);
+        } else {
+            // vice anchor can use a smaller size
+            // RATIO_4_3 & VIDEO_ENCODING_SIZE_HEIGHT_240 means the output size is 320 x 240
+            // 4:3 looks better in the mix frame
+            options.setVideoEncodingSizeRatio(RTCConferenceOptions.VIDEO_ENCODING_SIZE_RATIO.RATIO_4_3);
+            options.setVideoEncodingSizeLevel(RTCConferenceOptions.VIDEO_ENCODING_SIZE_HEIGHT_240);
+            options.setVideoBitrateRange(300 * 1024, 800 * 1024);
+            // 15 fps is enough
+            options.setVideoEncodingFps(15);
+        }
+        options.setHWCodecEnabled(!isSwCodec);
+        mRTCStreamingManager.setConferenceOptions(options);
+
+        if (mRole) {
+            mRTCStreamingManager.setStreamStatusCallback(mStreamStatusCallback);
+            mRTCStreamingManager.setStreamingStateListener(mStreamingStateChangedListener);
+            mRTCStreamingManager.setStreamingSessionListener(mStreamingSessionListener);
+            mStreamingProfile = new StreamingProfile();
+            mStreamingProfile.setVideoQuality(StreamingProfile.VIDEO_QUALITY_MEDIUM2)
+                    .setAudioQuality(StreamingProfile.AUDIO_QUALITY_MEDIUM1)
+                    .setEncoderRCMode(StreamingProfile.EncoderRCModes.QUALITY_PRIORITY)
+                    .setPreferredVideoEncodingSize(options.getVideoEncodingWidth(), options.getVideoEncodingHeight());
+            //if (isLandscape) {
+            //    mStreamingProfile.setEncodingOrientation(StreamingProfile.ENCODING_ORIENTATION.LAND);
+            //} else {
+                mStreamingProfile.setEncodingOrientation(StreamingProfile.ENCODING_ORIENTATION.PORT);
+            //}
+            mRTCStreamingManager.prepare(mStreamingProfile);
+        } else {
+            //mControlButton.setText("开始连麦");
+        }
+    }
+
+    private RTCConferenceStateChangedListener mRTCStreamingStateChangedListener = new RTCConferenceStateChangedListener() {
+        @Override
+        public void onConferenceStateChanged(RTCConferenceState state, int extra) {
+            switch (state) {
+                case READY:
+                    // You must `StartConference` after `Ready`
+                    showToast(getString(R.string.ready), Toast.LENGTH_SHORT);
+                    break;
+                case CONNECT_FAIL:
+                    showToast(getString(R.string.failed_to_connect_rtc_server), Toast.LENGTH_SHORT);
+                    finish();
+                    break;
+                case VIDEO_PUBLISH_FAILED:
+                case AUDIO_PUBLISH_FAILED:
+                    showToast(getString(R.string.failed_to_publish_av_to_rtc) + extra, Toast.LENGTH_SHORT);
+                    finish();
+                    break;
+                case VIDEO_PUBLISH_SUCCESS:
+                    showToast(getString(R.string.success_publish_video_to_rtc), Toast.LENGTH_SHORT);
+                    break;
+                case AUDIO_PUBLISH_SUCCESS:
+                    showToast(getString(R.string.success_publish_audio_to_rtc), Toast.LENGTH_SHORT);
+                    break;
+                case USER_JOINED_AGAIN:
+                    showToast(getString(R.string.user_join_other_where), Toast.LENGTH_SHORT);
+                    finish();
+                    break;
+                case USER_KICKOUT_BY_HOST:
+                    showToast(getString(R.string.user_kickout_by_host), Toast.LENGTH_SHORT);
+                    finish();
+                    break;
+                default:
+                    return;
+            }
+        }
+    };
+
+    private RTCUserEventListener mRTCUserEventListener = new RTCUserEventListener() {
+        @Override
+        public void onUserJoinConference(String remoteUserId) {
+            Log.d(TAG, "onUserJoinConference: " + remoteUserId);
+        }
+
+        @Override
+        public void onUserLeaveConference(String remoteUserId) {
+            Log.d(TAG, "onUserLeaveConference: " + remoteUserId);
+        }
+    };
+
+    private RTCRemoteWindowEventListener mRTCRemoteWindowEventListener = new RTCRemoteWindowEventListener() {
+        @Override
+        public void onRemoteWindowAttached(RTCVideoWindow window, String remoteUserId) {
+            Log.d(TAG, "onRemoteWindowAttached: " + remoteUserId);
+        }
+
+        @Override
+        public void onRemoteWindowDetached(RTCVideoWindow window, String remoteUserId) {
+            Log.d(TAG, "onRemoteWindowDetached: " + remoteUserId);
+        }
+
+        @Override
+        public void onFirstRemoteFrameArrived(String remoteUserId) {
+            Log.d(TAG, "onFirstRemoteFrameArrived: " + remoteUserId);
+        }
+    };
+
+    private StreamingStateChangedListener mStreamingStateChangedListener = new StreamingStateChangedListener() {
+        @Override
+        public void onStateChanged(final StreamingState state, Object o) {
+            switch (state) {
+                case PREPARING:
+                    setStatusText(getString(R.string.preparing));
+                    Log.d(TAG, "onStateChanged state:" + "preparing");
+                    break;
+                case READY:
+                    mIsInReadyState = true;
+                    setStatusText(getString(R.string.ready));
+                    Log.d(TAG, "onStateChanged state:" + "ready");
+                    break;
+                case CONNECTING:
+                    Log.d(TAG, "onStateChanged state:" + "connecting");
+                    break;
+                case STREAMING:
+                    setStatusText(getString(R.string.streaming));
+                    Log.d(TAG, "onStateChanged state:" + "streaming");
+                    break;
+                case SHUTDOWN:
+                    mIsInReadyState = true;
+                    setStatusText("Shut down");
+                    Log.d(TAG, "onStateChanged state:" + "shutdown");
+                    break;
+                case UNKNOWN:
+                    Log.d(TAG, "onStateChanged state:" + "unknown");
+                    break;
+                case SENDING_BUFFER_EMPTY:
+                    Log.d(TAG, "onStateChanged state:" + "sending buffer empty");
+                    break;
+                case SENDING_BUFFER_FULL:
+                    Log.d(TAG, "onStateChanged state:" + "sending buffer full");
+                    break;
+                case AUDIO_RECORDING_FAIL:
+                    Log.d(TAG, "onStateChanged state:" + "audio recording failed");
+                    showToast(getString(R.string.failed_open_microphone), Toast.LENGTH_SHORT);
+                    stopPublishStreaming();
+                    break;
+                case OPEN_CAMERA_FAIL:
+                    Log.d(TAG, "onStateChanged state:" + "open camera failed");
+                    showToast(getString(R.string.failed_open_camera), Toast.LENGTH_SHORT);
+                    stopPublishStreaming();
+                    break;
+                case IOERROR:
+                    /**
+                     * Network-connection is unavailable when `startStreaming`.
+                     * You can do reconnecting or just finish the streaming
+                     */
+                    Log.d(TAG, "onStateChanged state:" + "io error");
+                    showToast(getString(R.string.io_error), Toast.LENGTH_SHORT);
+                    sendReconnectMessage();
+                    // stopPublishStreaming();
+                    break;
+                case DISCONNECTED:
+                    /**
+                     * Network-connection is broken after `startStreaming`.
+                     * You can do reconnecting in `onRestartStreamingHandled`
+                     */
+                    Log.d(TAG, "onStateChanged state:" + "disconnected");
+                    setStatusText(getString(R.string.disconnected));
+                    // we will process this state in `onRestartStreamingHandled`
+                    break;
+            }
+        }
+    };
+
+    private StreamingSessionListener mStreamingSessionListener = new StreamingSessionListener() {
+        @Override
+        public boolean onRecordAudioFailedHandled(int code) {
+            return false;
+        }
+
+        /**
+         * When the network-connection is broken, StreamingState#DISCONNECTED will notified first,
+         * and then invoked this method if the environment of restart streaming is ready.
+         *
+         * @return true means you handled the event; otherwise, given up and then StreamingState#SHUTDOWN
+         * will be notified.
+         */
+        @Override
+        public boolean onRestartStreamingHandled(int code) {
+            Log.d(TAG, "onRestartStreamingHandled, reconnect ...");
+            return mRTCStreamingManager.startStreaming();
+        }
+
+        @Override
+        public Camera.Size onPreviewSizeSelected(List<Camera.Size> list) {
+            return null;
+        }
+    };
+
+    private StreamStatusCallback mStreamStatusCallback = new StreamStatusCallback() {
+        @Override
+        public void notifyStreamStatusChanged(final StreamingProfile.StreamStatus streamStatus) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String stat = "bitrate: " + streamStatus.totalAVBitrate / 1024 + " kbps"
+                            + "\naudio: " + streamStatus.audioFps + " fps"
+                            + "\nvideo: " + streamStatus.videoFps + " fps";
+                    Log.i(TAG, stat);
+                }
+            });
+        }
+    };
+
+    private void setStatusText(final String status) {
+        Log.i(TAG, "setStatusText: " + status);
+    }
+
+    private Toast mToast;
+    private void showToast(final String text, final int duration) {
+        if (mIsActivityPaused) {
+            return;
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                mToast = Toast.makeText(LiveRoomActivity.this, text, duration);
+                mToast.show();
+            }
+        });
+    }
+
+    public boolean startPublishStreaming() {
+        if (mIsPublishStreamStarted) {
+            return true;
+        }
+        //if (!mIsInReadyState) {
+        //    showToast(getString(R.string.stream_state_not_ready), Toast.LENGTH_SHORT);
+        //    return false;
+        //}
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String publishUrl = "{\"id\":\"z1.taqutest.sc4qjuawpip\",\"createdAt\":\"2017-06-13T11:12:04.386+08:00\",\"updatedAt\":\"2017-06-13T11:12:04.386+08:00\",\"expireAt\":\"0001-01-01T00:00:00Z\",\"title\":\"sc4qjuawpip\",\"hub\":\"taqutest\",\"disabledTill\":0,\"disabled\":false,\"publishKey\":\"8492ed67-3fa1-4186-b84a-4e913b9475e2\",\"publishSecurity\":\"static\",\"nonce\":0,\"hosts\":{\"publish\":{\"rtmp\":\"pili-publish.xiaobaoshu.com\"},\"live\":{\"hdl\":\"pili-live-hdl.xiaobaoshu.com\",\"hls\":\"pili-live-hls.xiaobaoshu.com\",\"http\":\"pili-live-hls.xiaobaoshu.com\",\"rtmp\":\"pili-live-rtmp.xiaobaoshu.com\",\"snapshot\":\"100008f.live1-snapshot.z1.pili.qiniucdn.com\"},\"playback\":{\"hls\":\"pili-playback.xiaobaoshu.com\",\"http\":\"pili-playback.xiaobaoshu.com\"},\"play\":{\"http\":\"pili-live-hls.xiaobaoshu.com\",\"rtmp\":\"pili-live-rtmp.xiaobaoshu.com\"}},\"nropEnable\":null}";
+                    mStreamingProfile.setStream(new StreamingProfile.Stream(new JSONObject(publishUrl)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                mRTCStreamingManager.setStreamingProfile(mStreamingProfile);
+                if (!mRTCStreamingManager.startStreaming()) {
+                    showToast(getString(R.string.failed_to_start_streaming), Toast.LENGTH_SHORT);
+                    Log.i(TAG, "startStreaming fail");
+                    return;
+                }
+                Log.i(TAG, "startStreaming");
+                mIsPublishStreamStarted = true;
+                if (mIsActivityPaused) {
+                    stopPublishStreaming();
+                    //return false;
+                }
+            }
+        });
+
+        return true;
+    }
+
+    public boolean isInReadyState() {
+        return mIsInReadyState;
+    }
+
+    public boolean isPublishStreamStarted() {
+        return mIsPublishStreamStarted;
+    }
+
+    public boolean stopPublishStreaming() {
+        if (!mIsPublishStreamStarted) {
+            return true;
+        }
+        mRTCStreamingManager.stopStreaming();
+        mIsPublishStreamStarted = false;
+        showToast(getString(R.string.stop_streaming), Toast.LENGTH_SHORT);
+        return false;
+    }
+
+    private static final int MESSAGE_ID_RECONNECTING = 0x01;
+
+    private void sendReconnectMessage() {
+        showToast("正在重连...", Toast.LENGTH_SHORT);
+        mHandler.removeCallbacksAndMessages(null);
+        mHandler.sendMessageDelayed(mHandler.obtainMessage(MESSAGE_ID_RECONNECTING), 500);
+    }
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what != MESSAGE_ID_RECONNECTING || mIsActivityPaused || !mIsPublishStreamStarted) {
+                return;
+            }
+            if (!isNetworkAvailable(getApplicationContext())) {
+                sendReconnectMessage();
+                return;
+            }
+            Log.d(TAG, "do reconnecting ...");
+            mRTCStreamingManager.startStreaming();
+        }
+    };
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    private void resumeQiniuRtc() {
+        mIsActivityPaused = false;
+        if (mRTCStreamingManager != null) {
+            mRTCStreamingManager.resume();
+        }
+    }
+
+    private void pauseQiniuRtc() {
+        mIsActivityPaused = true;
+        if (mRTCStreamingManager != null) {
+            mRTCStreamingManager.pause();
+        }
+
+    }
+
+    private void destroyQiniuRtc() {
+        mIsActivityPaused = true;
+        if (mRTCStreamingManager != null) {
+            mRTCStreamingManager.destroy();
+        }
+        RTCStreamingManager.deinit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        resumeQiniuRtc();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        pauseQiniuRtc();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        destroyQiniuRtc();
     }
 }
